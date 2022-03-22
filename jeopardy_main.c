f14cbc0003dc55d1d66c42d3472ba4eae18b8ab3
Jake Chirstensen <88994417+jchristensen60@users.noreply.github.com>
2022-03-22 14:27:18 -0700

    Update jeopardy_main.c

--- jeopardy_main.c
+++ jeopardy_main.c
@@ -16,7 +16,7 @@ void setBlue();
 void setYellow();
 void printBoard();
 void clearScreen(); // Code from Prof Marriott's 'movingImage.c' example
-//void printQuestion();
+void printQuestion(int row, int col);
 
 int main(void)
 {
