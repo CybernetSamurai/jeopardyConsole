51c520fe45246f3fb533b974959d0f262bd955df
Jake Chirstensen <88994417+jchristensen60@users.noreply.github.com>
2022-03-22 14:11:59 -0700

    Update and rename jeopardy_questions.txt to jeopardy_questions.c

---
+++ jeopardy_questions.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void printQuestion(int row, int col)
+{
+  char fileList[6][10] = {
+                            "category1",
+                            "category2",
+                            "category3",
+                            "category4",
+                            "category5",
+                            "category6"
+                         };
+  char cat;
+  
+  FILE* f = fopen(cat, "r");
+  if (f == NULL)
+    printf("Error opening file.\n");
+}
