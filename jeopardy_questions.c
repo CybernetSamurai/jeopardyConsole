10c7bf579fb4e426974fd07120deaa1ceccc5f16
Jake Chirstensen <88994417+jchristensen60@users.noreply.github.com>
2022-03-23 13:47:38 -0700

    Update jeopardy_questions.c

--- jeopardy_questions.c
+++ jeopardy_questions.c
@@ -2,8 +2,8 @@
 
 void printQuestion(int row, int col) {
    int temp, loop_cnt = 0;
-   char line[1000];
-   char fileList[6][14] = {
+   char line[300];
+   char file_list[6][14] = {
                               "category1.txt",
                               "category2.txt",
                               "category3.txt",
@@ -11,28 +11,32 @@ void printQuestion(int row, int col) {
                               "category5.txt",
                               "category6.txt"
                           };
+   char line_list[5][300];
   
    //fscanf(f, "%[^;]", line);
    //printf("%s\n", line);
    
    // Resource:
    // https://stackoverflow.com/questions/21114591/c-programming-read-specific-line-from-text-file?msclkid=8747c7afaae011eca26a4c7fa106a47d
-   FILE *f = fopen(fileList[col], "r");
+   FILE *f = fopen(file_list[col], "r");
    if ( f == NULL ) {
       printf("Error opening file.\n");
    }
    else {
-      while (fgets(line, sizeof line, f) != NULL) {
-         if (loop_cnt == row) {
-            printf("%s\n", line);
-            //use line or in a function return it
-            //in case of a return first close the file with "fclose(file);"
-         }
-         else {
-            ++loop_cnt;
-         }
+      while (fgets(line, 300, f) != NULL) {
+         line_list[loop_cnt] = line;
+         ++loop_cnt;
+         //if (loop_cnt == row) {
+         //   printf("%s\n", line);
+         //   //use line or in a function return it
+         //   //in case of a return first close the file with "fclose(file);"
+         //}
+         //else {
+         //   ++loop_cnt;
+         //}
       }
       fclose(f);
+      printf("%s\n", line_list[1]);
    }
    printf("yes or no");
    scanf("%d", &temp);
