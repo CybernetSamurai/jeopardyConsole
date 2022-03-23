532b3efe8cf5b6e39f62dc480e5a9ab560f5287f
Jake Chirstensen <88994417+jchristensen60@users.noreply.github.com>
2022-03-23 13:03:41 -0700

    Update jeopardy_questions.c

--- jeopardy_questions.c
+++ jeopardy_questions.c
@@ -1,49 +1,38 @@
 #include <stdio.h>
 
-void printQuestion(int row, int col)
-{
-  int temp, loop_cnt = 0, line_number;
-  char line_max[5];
-  char fileList[6][14] = {
-                            "category1.txt",
-                            "category2.txt",
-                            "category3.txt",
-                            "category4.txt",
-                            "category5.txt",
-                            "category6.txt"
-                         };
+void printQuestion(int row, int col) {
+   int temp, loop_cnt = 0;
+   char line_max[1000];
+   char fileList[6][14] = {
+                              "category1.txt",
+                              "category2.txt",
+                              "category3.txt",
+                              "category4.txt",
+                              "category5.txt",
+                              "category6.txt"
+                          };
   
   //if (f == NULL) printf("Error opening file.\n");
-  // reads text until newline is encountered
   //fscanf(f, "%[^;]", line);
   //printf("%s\n", line);
-  //fclose(f);
-  //printf("yes or no");
-  //scanf("%d", &temp);
   
-  FILE *f = fopen(fileList[col], "r");
-  if ( f != NULL )
-  {
-      while (fgets(line_max, 6, f) != NULL) /* read a line */
-      {
-          if (loop_cnt == row)
-          {
-            printf("%s\n", line_max);
-              //use line or in a function return it
-              //in case of a return first close the file with "fclose(file);"
-          }
-          else
-          {
-              ++loop_cnt;
-          }
+   FILE *f = fopen(fileList[col], "r");
+   if ( f == NULL ) {
+      printf("Error opening file.\n");
+   }
+   else {
+      while (fgets(line_max, 1000, f) != NULL) {
+         if (loop_cnt == row) {
+             printf("%s\n", line_max);
+               //use line or in a function return it
+               //in case of a return first close the file with "fclose(file);"
+         }
+         else {
+            ++loop_cnt;
+         }
       }
       fclose(f);
-  }
-  else
-  {
-      //file doesn't exist
-    printf("Error opening file.\n");
-  }
-  printf("yes or no");
-  scanf("%d", &temp);
+   }
+   printf("yes or no");
+   scanf("%d", &temp);
 }
