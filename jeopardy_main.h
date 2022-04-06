eeb90f1d4590e96dbf087c656ded72e399601a56
Jake Chirstensen <88994417+jchristensen60@users.noreply.github.com>
2022-04-06 12:42:04 -0700

    Update jeopardy_main.h

--- jeopardy_main.h
+++ jeopardy_main.h
@@ -1,6 +1,6 @@
 /* Definition and Prototype Function for jeopardy_main.c */
-#define ROWS 5 // Number of Point Selections
-#define COLS 6 // Number of Categories
+#define ROWS 5       // Number of Point Selections
+#define COLS 6       // Number of Categories
 #define MAX_PTS 9999 // Maximum Number of Points a Team can Earn
 
 bool optionSelectionValidation(int selection);
@@ -16,12 +16,12 @@ void clearScreen(); // Code from Prof Marriott's 'movingImage.c' example
 int convertPointsSelected(int points);
 
 /* Definition and Prototype Function for jeopardy_questions.c */
-#define MAX_STR_LEN 250 // Maximum Length of Questions/Answers
-#define FILE_NUM 6 // Number of Category Text Files
-#define FILE_LEN 14 // Length of Category Text File Name
-#define QUESTION_CNT 5 // Number of Questions per Category Text File
+#define MAX_STR_LEN 250  // Maximum Length of Questions/Answers
+#define FILE_NUM 6       // Number of Category Text Files
+#define FILE_LEN 14      // Length of Category Text File Name
+#define QUESTION_CNT 5   // Number of Questions per Category Text File
 #define QUESTION_TIME 10 // Length of Question Timer
-#define BOARD_WIDTH 44 // Length of Game Board for Question Formatting
+#define BOARD_WIDTH 44   // Length of Game Board for Question Formatting
 
 void selectCatagoryFile(int col);
 void getQuestionAnswerArray(int col, int row);
