f7f48312e3439032256c2706453b9b89292155ae
Jake Chirstensen <88994417+jchristensen60@users.noreply.github.com>
2022-04-06 00:11:29 -0700

    Update jeopardy_main.c

--- jeopardy_main.c
+++ jeopardy_main.c
@@ -11,24 +11,24 @@
 //#include "jeopardy_questions.h"
 #include "jeopardy_main.h"
 
-#define ROWS 5
-#define COLS 6
-#define MAX_PTS 9999
+//#define ROWS 5
+//#define COLS 6
+//#define MAX_PTS 9999
 
-bool optionSelectionValidation();
-void addPoints();
-bool printAnswerValidation();
-void saveEnteredData();
-void resetBoard();
-int teamSelect();
-bool teamSelectValidation();
-int mainMenuUserInput(char* type);
-bool colSelectValidation(int col);
-bool pointSelectValidation(int pointSelect);
-void setColor(char* color);
-void printBoard();
-void clearScreen(); // Code from Prof Marriott's 'movingImage.c' example
-int convertPointsSelected(int points);
+//bool optionSelectionValidation();
+//void addPoints();
+//bool printAnswerValidation();
+//void saveEnteredData();
+//void resetBoard();
+//int teamSelect();
+//bool teamSelectValidation();
+//int mainMenuUserInput(char* type);
+//bool colSelectValidation(int col);
+//bool pointSelectValidation(int pointSelect);
+//void setColor(char* color);
+//void printBoard();
+//void clearScreen(); // Code from Prof Marriott's 'movingImage.c' example
+//int convertPointsSelected(int points);
 
 // Global Variables for Team Scores
 int Team_One = 0;
