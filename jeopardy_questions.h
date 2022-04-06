fc83c3cad49c251b729b57ec1bd407badd70da68
Jake Chirstensen <88994417+jchristensen60@users.noreply.github.com>
2022-04-06 00:06:50 -0700

    Update jeopardy_questions.h

--- jeopardy_questions.h
+++ jeopardy_questions.h
@@ -1,1 +1,11 @@
-void printQuestion(int row, int col);
+#define MAX_STR_LEN 250
+#define FILE_NUM 6
+#define FILE_LEN 14
+#define QUESTION_CNT 5
+#define QUESTION_TIME 10
+#define BOARD_WIDTH 44
+
+void selectCatagoryFile(int col);
+void getQuestionAnswerArray(int col, int row);
+void printQuestion(int col, int row);
+int printAnswer(int col, int row);
