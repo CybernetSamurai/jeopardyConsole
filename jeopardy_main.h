712466f41076d3be8a3ae4626cd5fb5f179d8777
Jake Chirstensen <88994417+jchristensen60@users.noreply.github.com>
2022-04-06 00:03:40 -0700

    Update jeopardy_main.h

--- jeopardy_main.h
+++ jeopardy_main.h
@@ -1,19 +1,15 @@
-
 #define ROWS 5
 #define COLS 6
 #define MAX_PTS 9999
 
-bool optionSelectionValidation();
-void addPoints();
-bool printAnswerValidation();
-void saveEnteredData();
-void resetBoard();
-int teamSelect();
-bool teamSelectValidation();
+bool optionSelectionValidation(int selection);
+void addPoints(int teamNumber, int ptsSelected, int selection);
+void saveEnteredData(int team, int cat, int point);
+void resetBoard(int arrayPoints[ROWS][COLS], int arraySelect[ROWS][COLS]);
 int mainMenuUserInput(char* type);
 bool colSelectValidation(int col);
-bool pointSelectValidation(int pointSelect);
+bool pointSelectValidation(int points);
 void setColor(char* color);
-void printBoard();
+void printBoard(int arrayPoints[ROWS][COLS], int arraySelect[ROWS][COLS]);
 void clearScreen(); // Code from Prof Marriott's 'movingImage.c' example
 int convertPointsSelected(int points);
