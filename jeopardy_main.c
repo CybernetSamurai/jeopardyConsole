8567f94ddd1d5785e41cba4d83717cd25b9ca10d
Jake Chirstensen <88994417+jchristensen60@users.noreply.github.com>
2022-04-06 14:44:52 -0700

    Update jeopardy_main.c

--- jeopardy_main.c
+++ jeopardy_main.c
@@ -239,19 +239,15 @@ void printBoard(int arrayPoints[ROWS][COLS], int arraySelect[ROWS][COLS]) {
     }
     setColor("Blue");
     printf("---------------------------------------------------------\n"
-           " ---------- Team One [");
-    highlightNumber(1, "Red");
-    printf("] ------- Team Two [");
-    highlightNumber(2, "Red");
-    printf("] ---------- \n"
+           " ---------- Team One ["); HLN(1, "Red"); printf("] ------- Team Two ["); HLN(2, "Red"); printf("] ---------- \n"
            "---------------------------------------------------------\n");
 }
 
-void highlightNumber(int input, char* color) {
+typedef void highlightNumber(int input, char* color) {
     setColor(color);
     printf("%d", input);
     setColor("Blue");
-}
+} HLN;
 
 // Clear Game Screen
 void clearScreen() {
