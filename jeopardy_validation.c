ee6e17cec91c4fe88ba77851eabe971fed4bfc83
Jake Chirstensen <88994417+jchristensen60@users.noreply.github.com>
2022-04-06 18:04:54 -0700

    Update jeopardy_validation.c

--- jeopardy_validation.c
+++ jeopardy_validation.c
@@ -1,1 +1,9 @@
+// Validate selection between two options
+bool optionSelectionValidation(int selection) {
+    bool check = false;
+    if (selection == 1 || selection == 2) {
+        check = true;
+    }
+    return check;
+}
 
