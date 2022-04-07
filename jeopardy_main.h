ae08f7be4fc5708386c2e549c52e67586feb225f
Jake Chirstensen <88994417+jchristensen60@users.noreply.github.com>
2022-04-06 18:30:08 -0700

    Update jeopardy_main.h

--- jeopardy_main.h
+++ jeopardy_main.h
@@ -8,8 +8,8 @@
 //#include <stdlib.h>
 
 // Global Variables for Team Scores
-int Team_One = 0;
-int Team_Two = 0;
+//int Team_One = 0;
+//int Team_Two = 0;
 
 /* Definition and Prototype Function for jeopardy_main.c */
 #define ROWS 5       // Number of Point Selections
