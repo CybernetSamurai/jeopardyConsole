614c44d8fd763f3335fd877c156e52369e6e4070
Jake Chirstensen <88994417+jchristensen60@users.noreply.github.com>
2022-04-07 15:52:27 -0700

    Update jeopardy_questions.c

--- jeopardy_questions.c
+++ jeopardy_questions.c
@@ -48,8 +48,8 @@ void getQuestionAnswerArray(int col, int row) {
     // Nullify question/answer Strings
     
     for (i = 0; i < MAX_STR_LEN; ++i) {
-        question[i] = '\0';
-        answer[i] = '\0';
+        question[i] = ' ';
+        answer[i] = ' ';
     }
     
     // Save the "Question" Part of the Line in Seperate Variable
