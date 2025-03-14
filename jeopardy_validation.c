cc4717a64ebbef0a7bbfd2fc7ac70565fb818e4c
Jake Christensen <88994417+Cy3brSamurai@users.noreply.github.com>
2025-03-14 22:58:17 +0000

    Update jeopardy_validation.c

--- jeopardy_validation.c
+++ jeopardy_validation.c
@@ -1,6 +1,5 @@
 /**************************************
 * File:     jeopardy_validation.c
-* Group:    Jacob, Jackson, Annabelle
 * Authors:  Jacob Christensen
 **************************************/
 
